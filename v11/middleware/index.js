ac83e47e25d30271ee2c856adac724eb3dc5a213
Manjeet <kumar.manjeet13@gmail.com>
2018-06-29 14:54:19 +0530

    push final deployed version

---
+++ v11/middleware/index.js
@@ -0,0 +1,61 @@
+var Campground  = require("../models/campground");
+var Comment     = require("../models/comment");
+
+// all the middleware goes here
+
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, camp){
+            if(err){
+                req.flash("error", "campground not found");
+                res.redirect("back");
+            }else{
+                //does the user own the campground?
+                if(camp.author.id.equals(req.user._id)){
+                    next();
+                } else{
+                    req.flash("error", "You don't have permission to do that!");
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        req.flash("error", "You need to be logged in to do that!");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                req.flash("error", "comment not found");
+                res.redirect("back");
+            }else{
+                //does the user own the campground?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else{
+                    req.flash("error", "You don't have permission to do that");
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that!");
+    res.redirect("/login");
+};
+
+
+module.exports = middlewareObj;
